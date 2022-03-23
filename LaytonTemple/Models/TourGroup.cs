a7932a5365e52fb7985b69d48f8dc528c7992b41
csalvesen84 <73505297+csalvesen84@users.noreply.github.com>
2022-03-23 15:40:32 -0600

    main

---
+++ LaytonTemple/Models/TourGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTemple.Models
+{
+    public class TourGroup
+    {
+        [Key]
+        [Required]
+        public int GroupId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public long? Phone { get; set; }
+
+        public string Timeslot { get; set; }
+
+    }
+}
